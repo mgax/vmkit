b9934aeea6635fe3ebb3a5f11e3c492647d3d9d4
Alex Morega <alex@grep.ro>
2016-12-05 14:25:32 +0200

    rewrite image builder as python script

---
+++ vmkit.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+
+import sys
+from pathlib import Path
+import argparse
+from io import BytesIO
+from tempfile import TemporaryDirectory
+import subprocess
+
+def run(cmdargs, stdin_data=b'', **kwargs):
+    kwargs.setdefault('stdin', subprocess.PIPE)
+    p = subprocess.Popen([str(a) for a in cmdargs], **kwargs)
+    p.communicate(stdin_data)
+    if p.returncode != 0:
+        raise RuntimeError("Subprocess failed: {!r}".format(cmdargs))
+
+def patchiso(orig, iso):
+    repo = Path(__file__).resolve().parent
+
+    with TemporaryDirectory() as tmp:
+        dist = Path(tmp) / 'dist'
+
+        print('copying contents of', orig, 'to', dist)
+        run(['cp', '-a', orig, dist])
+
+        patch_path = repo / 'console-ttyS0.patch'
+        print('applying patch', patch_path)
+        with patch_path.open('rb') as patch:
+            run(['patch', '-d', dist, '-p1'], stdin=patch)
+
+        initrd_gz = dist / 'install.amd' / 'initrd.gz'
+        initrd = dist / 'install.amd' / 'initrd'
+        print('patching', initrd_gz)
+        run(['gunzip', initrd_gz])
+        run(['cpio', '-o', '-H', 'newc', '-A', '-F', initrd],
+            cwd=str(repo), stdin_data=b'preseed.cfg\n')
+        run(['gzip', initrd])
+
+        print('creating ISO image', iso)
+        run([
+          'xorriso', '-as', 'mkisofs',
+          '-r', '-J', '-joliet-long',
+          '-no-emul-boot',
+          '-boot-load-size', '4',
+          '-boot-info-table',
+          '-b', 'isolinux/isolinux.bin',
+          '-c', 'isolinux/boot.cat',
+          '-o', iso,
+          dist,
+        ])
+
+def parser_for_patchiso(parser):
+    parser.add_argument('orig')
+    parser.add_argument('-o', '--output')
+    parser.set_defaults(handler=lambda o: patchiso(o.orig, o.output))
+
+def main():
+    parser = argparse.ArgumentParser()
+    commands = parser.add_subparsers()
+
+    parser_for_patchiso(commands.add_parser('patchiso'))
+
+    options = parser.parse_args()
+    options.handler(options)
+
+if __name__ == '__main__':
+    main()
