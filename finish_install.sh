d0a48dedfa23ddc36b9dda06d415cc15d762cbaf
Alex Morega <alex@grep.ro>
2016-12-07 11:12:05 +0200

    fix paths

--- finish_install.sh
+++ finish_install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -e
 
-cd /target/etc/systemd/system
-mkdir 'serial-getty@ttyS0.service.d'
-cat > 'serial-getty@ttyS0.service.d/override.conf' <<EOF
+cd /target
+mkdir 'etc/systemd/system/serial-getty@ttyS0.service.d'
+cat > 'etc/systemd/system/serial-getty@ttyS0.service.d/override.conf' <<EOF
 [Service]
 ExecStart=
 ExecStart=-/sbin/agetty --autologin root --noclear %I $TERM
 EOF
 
-echo '/sbin/poweroff' > /root/.bash_logout
+echo '/sbin/poweroff' > root/.bash_logout
